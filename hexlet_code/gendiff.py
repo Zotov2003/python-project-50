7e42425117a65ae79fc385288444d9db60a3a4f3
Максим Порошин <baiter159159@gmail.com>
2024-09-04 21:38:12 +0500

    flake

--- hexlet_code/gendiff.py
+++ hexlet_code/gendiff.py
@@ -7,9 +7,9 @@ def generate_diff(filepath1, filepath2):
         data2 = json.load(file2)
 
     keys = sorted(set(data1.keys()).union(data2.keys()))
-    
+
     diff = []
-    
+
     for key in keys:
         if key in data1 and key not in data2:
             diff.append(f"- {key}: {json.dumps(data1[key])}")
@@ -20,10 +20,10 @@ def generate_diff(filepath1, filepath2):
             diff.append(f"+ {key}: {json.dumps(data2[key])}")
         else:
             diff.append(f"  {key}: {json.dumps(data1[key])}")
-    
+
     diff_str = "{\n"
     for line in diff:
         diff_str += f"  {line}\n"
     diff_str += "}\n"
-    
+
     return diff_str
--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,12 +1,13 @@
 import argparse
 from gendiff import generate_diff
 
+
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument("first_file", type=str, help="")
     parser.add_argument("second_file", type=str, help="")
-    parser.add_argument("-f", "--format", type=str, 
+    parser.add_argument("-f", "--format", type=str,
                         help="set format of output")
 
     args = parser.parse_args()
