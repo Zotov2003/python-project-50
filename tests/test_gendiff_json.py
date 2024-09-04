0b562d575433cd2000f82c0321e8cbe9394d48e1
Максим Порошин <baiter159159@gmail.com>
2024-09-04 18:54:56 +0500

    add tests

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -9,28 +9,36 @@ def file1_path():
 def file2_path():
     return 'tests/fixtures/file2.json'
 
+@pytest.fixture
+def empty_path():
+    return 'tests/fixtures/empty.json'
+
 def test_generate_diff(file1_path, file2_path):
-    expected = """{
+    expected = '''{
+  - follow: false
+    host: "hexlet.io"
+  - proxy: "123.234.53.22"
   - timeout: 50
   + timeout: 20
   + verbose: true
-}"""
+}
+'''
     assert generate_diff(file1_path, file2_path).strip() == expected.strip()
 
 def test_identical_files(file1_path):
-    # Тест на идентичные файлы
-    assert generate_diff(file1_path, file1_path).strip() == "{}"
-
-def test_empty_and_non_empty_files():
-    # Пример для пустого файла и файла с данными
-    empty_file_path = 'tests/fixtures/empty.json'
-    filled_file_path = 'tests/fixtures/file1.json'
-    
     expected = """{
-  - timeout: 50
-  - proxy: 123.234.53.22
-  - follow: false
-  - host: hexlet.io
+    follow: false
+    host: "hexlet.io"
+    proxy: "123.234.53.22"
+    timeout: 50
 }"""
-    
-    assert generate_diff(empty_file_path, filled_file_path).strip() == expected.strip()
+    assert generate_diff(file1_path, file1_path).strip() == expected.strip()
+
+def test_one_empty_file(empty_path, file1_path):
+    expected = """{
+  + follow: false
+  + host: "hexlet.io"
+  + proxy: "123.234.53.22"
+  + timeout: 50
+}"""
+    assert generate_diff(empty_path, file1_path).strip() == expected.strip()
