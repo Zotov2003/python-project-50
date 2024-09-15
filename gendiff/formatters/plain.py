f8ac41b9d794a156db601a56070aa1e9369633f0
Максим Порошин <baiter159159@gmail.com>
2024-09-15 20:20:55 +0500

    fix project6

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -39,6 +39,6 @@ def convert_value(value):
     if isinstance(value, (list, dict)):
         return '[complex value]'
     return (
-        str(f"{value}").lower() if isinstance(value, bool)
+        str(f"{value}").lower() if isinstance(value, bool) or isinstance(value, int)
         else f"'{value}'"
     )
